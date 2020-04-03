260ddc82d14c3faf9f36f87bc91ceb73494f5493
Federico Agarinis <federicof1994@gmail.com>
2020-04-03 11:10:58 +0200

    Riordionamento e pulizia del codice, rimozione container, row e col nelle table. fix vari

--- js/fly_monitor-ajax.js
+++ js/fly_monitor-ajax.js
@@ -6,12 +6,12 @@ $(document).ready(function () {
 	listLanded();
 });
 
-function doCall(typeRequest, urlPath, parametri, callbackOnSuccess, callbackOnError, isSpinnerShown) {
+function doCall(typeRequest, urlPath, parametri, callbackOnSuccess, callbackOnError, spinnerName) {
 	const HOST = 'http://212.237.32.76:3001';
 	const AJAX_TIMEOUT = 10000;
 
-	if (isSpinnerShown) {
-		showSpinner();
+	if (spinnerName) {
+		showSpinnerByName(spinnerName);
 	}
 
 	$.ajax({
@@ -23,64 +23,20 @@ function doCall(typeRequest, urlPath, parametri, callbackOnSuccess, callbackOnEr
 		success: callbackOnSuccess,
 		error: callbackOnError,
 		complete: function (jqXHR, textStatus) {
-			if (isSpinnerShown && textStatus !="pending") {
-				hideSpinner(1000);
+			if (spinnerName && textStatus != "pending") {
+				hideSpinnerByName(spinnerName, 1000);
 			}
 		},
 		timeout: AJAX_TIMEOUT
 	});
 }
 
-function listCreated() {
-	clearErroreVolo();
-
+function disableListButton(){
 	$("#listButtonId").prop("disabled", true);
-	doCall('GET', '/list', undefined, function (resultJson) {
-		buildCreatedTable(resultJson);
-		$("#listButtonId").prop("disabled", false);
-	},
-		function () {
-			$("#listButtonId").prop("disabled", false);
-		},
-		true);
 }
 
-function listLanded() {
-	doCall('GET', '/status', undefined, function (resultJson) {
-		buildLandedTable(resultJson, memorizedCurrentPage, undefined);
-	});
-}
-
-function showSpinner() {
-	$("[name='spinner']").show();
-}
-
-function hideSpinner(hideDelay) {
-	var spinner = $("[name='spinner']");
-	setTimeout(function () {
-		spinner.hide()
-	}, hideDelay);
-}
-
-function fly(voloId) {
-	clearErroreVolo();
-	clearInterval(autoRefresh);
-
-	doCall('GET', '/start/' + voloId, undefined,
-		function (resultJson) { //success
-			getCreatedTableRow(voloId).remove();
-			listLanded();
-			autoRefresh = setInterval(listLanded, REFRESH_TIME);
-		},
-		function (xhr, textStatus) { //error
-			if (xhr.status == 422) { //nel caso l'aereo e' gia' partito
-				setErroreVolo('errore di sincronizzazione: Volo [ID: ' + voloId + '] gia partito');
-				getCreatedTableRow(voloId).remove();
-			}
-			listLanded();
-			autoRefresh = setInterval(listLanded, REFRESH_TIME);
-		}
-	)
+function enableListButton(){
+	$("#listButtonId").prop("disabled", false);
 }
 
 function setErroreVolo(error) {
@@ -91,6 +47,62 @@ function clearErroreVolo() {
 	setErroreVolo('');
 }
 
+function listCreated() {
+	clearErroreVolo();
+
+	disableListButton()
+	doCall('GET', '/list', undefined, 
+		function onSuccess(resultJson) {
+			buildCreatedTable(resultJson);
+			enableListButton();
+	},	function  onError() {
+			enableListButton();
+	}, 'spinner');
+}
+
+function listLanded() {
+	doCall('GET', '/status', undefined, function onSuccess(resultJson) {
+		buildLandedTable(resultJson, memorizedCurrentPage);
+	}, undefined, 'spinnerLanded');
+}
+
+function showSpinnerByName(spinnerName) {
+	$("[name='"+spinnerName+"']").show();
+}
+
+function hideSpinnerByName(spinnerName, hideDelay) {
+	let spinner = $("[name='"+spinnerName+"']");
+	setTimeout(function () {
+		spinner.hide();
+	}, hideDelay);
+}
+
 function getCreatedTableRow(voloId) {
 	return $('tr[data-voloId =' + voloId + ']');
 }
+
+function removeCreatedTableRow(voloId){
+	getCreatedTableRow(voloId).remove();
+}
+
+function fly(voloId) {
+	clearErroreVolo();
+	clearInterval(autoRefresh);
+
+	doCall('GET', '/start/' + voloId, undefined,
+		function onSuccess(resultJson) {
+			removeCreatedTableRow(voloId);
+			listLanded();
+			autoRefresh = setInterval(listLanded, REFRESH_TIME);
+		},
+		function onError(xhr, textStatus) {
+			if (xhr.status == 422) { //nel caso l'aereo e' gia' partito
+				setErroreVolo('errore di sincronizzazione: Volo [ID: ' + voloId + '] gia partito');
+				removeCreatedTableRow(voloId);
+			}
+			listLanded();
+			autoRefresh = setInterval(listLanded, REFRESH_TIME);
+		}
+	)
+}
+
--- js/fly_monitor-table.builder.js
+++ js/fly_monitor-table.builder.js
@@ -1,33 +1,34 @@
+//-----------Globals Variables-----------//
 var memorizedLandedResultJson;
 var memorizedCurrentPage = 1;
 var memorizedtotalPages;
+//---------------------------------------//
 
-function buildCreatedTable(resultJson) {
-	clearCreatedTable();
-	let resultTable = $("#createdTableId");
-
-	for (let i = 0; i < resultJson.length; i++) {
-		resultTable.append(buildCreatedTableRow(resultJson[i]));
-	}
+function createTableRow() {
+	return $('<tr>');
 }
 
-function clearCreatedTable() {
-	$("#createdTableId").empty();
+function createTableData(innerHTML) {
+	return $('<td>').append($('<small>').html(innerHTML));
 }
 
-function buildCreatedTableRow(jsonEntry) {
-	let { _id, tratta, status } = jsonEntry;
+//-----------Created Table---------------//
+function buildCreatedTableRow(voloJSON) {
+	let tableRow = createTableRow().attr('data-voloId', voloJSON._id);
 
-	if (status.toUpperCase() == 'CREATED') {
-		status = createFlyButton(jsonEntry._id);
-	}
-
-	let tableRow = $('<tr>').attr('data-voloId', _id).addClass('row');
-
-	[_id, tratta, status].forEach(param =>
-		tableRow.append($('<td>').addClass('col col-2').append($('<small>').html(param))));
+	[voloJSON._id, voloJSON.tratta, createFlyButton(voloJSON._id)]
+		.forEach(param => tableRow.append(createTableData(param)));
 
 	return tableRow;
+}
+
+function emptyCreatedTable() {
+	return $("#createdTableId").empty();
+}
+
+function buildCreatedTable(JSON) {
+	let CreatedTable = emptyCreatedTable();
+	JSON.forEach(voloJSON => CreatedTable.append(buildCreatedTableRow(voloJSON)));
 }
 
 function createFlyButton(idVolo) {
@@ -41,116 +42,154 @@ function createFlyButton(idVolo) {
 	return flyButton;
 }
 
-function buildLandedTable(resultJson, selectedPage = 1, pageSize = 10) {
-	clearLandedTable();
-	var resultTable = $("#landedTableId");
-
-	if (resultJson) {
-		resultJson = sortJsonByStartDate(resultJson);
-		memorizedLandedResultJson = resultJson;
-	}
-	else {
-		resultJson = memorizedLandedResultJson;
-	}
-
-	let startIndex = (selectedPage - 1) * pageSize;
-	let endIndex = startIndex + pageSize;
-	for (let i = startIndex; i < resultJson.length && i < endIndex; i++) {
-		resultTable.append(buildLandedTableRow(resultJson[i]));
-	}
-
-	let totalRows = resultJson.length;
-	memorizedtotalPages = Math.ceil(totalRows / pageSize);
-
-	buildLandedTableNavigationSection(memorizedtotalPages, selectedPage);
+//-----------Landed/Flying Table----------//
+function emptyLandedTable() {
+	return $("#landedTableId").empty();
 }
 
-function selectPage(page){
-	if(page == null){
-		return;
-	}
-	memorizedCurrentPage = page;
-	$("nav[aria-label = landedTableNavigation] ul li[value ="+page+"]").addClass('active');
-}
-
-function sortJsonByStartDate(json){
+function sortJsonByStartDate(json) {
 	return json.sort(function (a, b) {
 		return (new Date(b.startDate)) - (new Date(a.startDate));
 	});
 }
 
-function buildLandedTableNavigationSection(totalPages, selectedPage, pageGroupSize = 15) {
-	$("nav[aria-label = landedTableNavigation]").remove();
-
-	let navigationSection = $("<nav>").attr('aria-label', 'landedTableNavigation');
-	let list = $('<ul>').addClass('pagination justify-content-center');
-	navigationSection.append(list);
-	
-	let startPage = Math.max(1, selectedPage - Math.ceil(pageGroupSize/2));
-	let endPage = Math.min(totalPages, startPage + pageGroupSize);
-
-	let numberOfPagesShown = endPage - startPage
-	if(numberOfPagesShown < pageGroupSize){
-		let pagesMissing = pageGroupSize - numberOfPagesShown;
-		startPage = Math.max(1, startPage - (pagesMissing));
-	}
-	
-	list.append(createChangePageButton('First', 1, totalPages));
-	list.append(createChangePageButton('Previous', selectedPage-1, totalPages));
-	for (let i = startPage; i <= endPage; i++) {
-		let listItem = $("<li>").addClass('page-item w-25').val(i);
-		listItem.append($('<a>').addClass('page-link').html(i));
-		listItem.click(function () {
-			buildLandedTable(undefined, i);
-		});
-
-		list.append(listItem);
-	}
-	list.append(createChangePageButton('Next', selectedPage+1, totalPages));
-	list.append(createChangePageButton('Last', totalPages, totalPages));
-
-	$("#landedTableId").after(navigationSection);
-	selectPage(selectedPage);
-	
-	return navigationSection;
+function isValidPage(page) {
+	return page >= 1 && page <= memorizedtotalPages;
 }
 
-function createChangePageButton(innerHTML, pageOnClick, totalPages){
+function landedTableChangePage(page) {
+	if (!isValidPage(page)) {
+		return;
+	}
+	buildLandedTable(undefined, page);
+}
+
+function tripleDotAnimation() {
+	return '<span class="one">.</span><span class="two">.</span><span class="three">.</span> '
+}
+
+function buildLandedTableRow(voloJSON) {
+	let { _id, tratta, status, startDate, endDate } = voloJSON;
+
+	let tableRow = createTableRow();
+
+	if (status.toUpperCase() == 'FLYING') {
+		tableRow.addClass('bg-success');
+		status += tripleDotAnimation() + '&#9992;';
+	}
+
+	[_id, tratta, status, startDate, endDate]
+		.forEach(param => tableRow.append(createTableData(param)));
+
+	return tableRow;
+}
+
+function calculateStartAndEndLandedRowIndex(selectedPage, pageSize, totalRows){
+	let startRowIndex = Math.max((selectedPage - 1) * pageSize, 1);
+	let endRowIndex = Math.min(startRowIndex + pageSize, totalRows - 1);
+
+	return [startRowIndex, endRowIndex];
+
+}
+
+function buildLandedTable(JSON, selectedPage = 1, pageSize = 10) {
+	var landedTable = emptyLandedTable();
+
+	if (JSON) { //ricostruisci la tabella se viene passato un JSON
+		JSON = sortJsonByStartDate(JSON);
+		memorizedLandedResultJson = JSON;
+	}
+	else { 		//altrimenti usa il JSON memorizzato precedentemente
+		JSON = memorizedLandedResultJson;
+	}
+
+	let totalRows = JSON.length;
+	memorizedtotalPages = Math.ceil(totalRows / pageSize);
+
+	let [startRowIndex, endRowIndex] = calculateStartAndEndLandedRowIndex(selectedPage, pageSize, totalRows);
+
+	for (let i = startRowIndex; i < endRowIndex; i++) {
+		landedTable.append(buildLandedTableRow(JSON[i]));
+	}
+
+	buildLandedTableNavigationSection(selectedPage);
+}
+
+//--------Navigation Section------------//
+
+function emptyNavigationSection() {
+	$("nav[aria-label = landedTableNavigation]").remove();
+	return $("<nav>").attr('aria-label', 'landedTableNavigation');
+}
+
+function createChangePageButton(innerHTML, pageOnClick) {
 	let button = $("<li>").addClass('page-item').append($('<a>').addClass('page-link').html(innerHTML));
-	if(pageOnClick <= 0 || pageOnClick > totalPages){
+	if (!isValidPage(pageOnClick)) {
 		return button.addClass('disabled');
 	}
 
-	button.click(function(){
-		if(pageOnClick <= 0 || pageOnClick > totalPages){
-			return;
-		}
-		buildLandedTable(undefined, pageOnClick);
+	button.click(function () {
+		landedTableChangePage(pageOnClick);
 	});
 
 	return button;
 }
 
-function clearLandedTable() {
-	$("#landedTableId").empty();
+function calculateStartAndEndPages(selectedPage, numberOfPageButtons) {
+	let startPage = Math.max(1, selectedPage - Math.ceil(numberOfPageButtons / 2));
+	let endPage = Math.min(memorizedtotalPages, startPage + numberOfPageButtons);
+
+	startPage = Math.max(1, Math.min(startPage, endPage - numberOfPageButtons));
+
+	return [startPage, endPage];
 }
 
-function buildLandedTableRow(jsonEntry) {
-	let { _id, tratta, status, startDate, endDate } = jsonEntry;
+function buildNavigationButtonList(selectedPage, numberOfPageButtons) {
+	let navButtonList = $('<ul>').addClass('pagination justify-content-center');
+	let [startPage, endPage] = calculateStartAndEndPages(selectedPage, numberOfPageButtons);
 
-	let tableRow = $('<tr>').addClass('row');
+	navButtonList.append(createChangePageButton('First', 1).attr('name','first'));
+	navButtonList.append(createChangePageButton('Previous', selectedPage - 1));
+	for (let i = startPage; i <= endPage; i++) {
+		let listItem = createChangePageButton(i, i).val(i).addClass('w-25');
+		navButtonList.append(listItem);
+	}
+	navButtonList.append(createChangePageButton('Next', selectedPage + 1));
+	navButtonList.append(createChangePageButton('Last', memorizedtotalPages).attr('name','last'));
 
-	if (status.toUpperCase() == 'FLYING') {
-		tableRow.addClass('bg-success');
-		status += tripleDotAnimation() + '&#9992;'; 
+	return navButtonList;
+}
+
+function disableButtonByName(name){
+	return $("nav[aria-label = landedTableNavigation] ul li[name =" + name + "]").addClass('disabled');
+}
+
+function ActivatePageButton(page) {
+	if (page == null) {
+		return;
+	}
+	
+	$("nav[aria-label = landedTableNavigation] ul li[value =" + page + "]").addClass('active');
+
+	if(page == 1){
+		disableButtonByName('first');
+	}
+	else if(page == memorizedtotalPages){
+		disableButtonByName('last');
 	}
 
-	[_id, tratta, status, startDate, endDate].forEach(param =>
-		tableRow.append($('<td>').addClass('col col-2').append($('<small>').html(param))));
-
-	return tableRow;
+	memorizedCurrentPage = page;
 }
 
-function tripleDotAnimation(){
-	return '<span class="one">.</span><span class="two">.</span><span class="three">.</span> '
+function buildLandedTableNavigationSection(selectedPage, numberOfPageButtons = 15) {
+	let navigationSection = emptyNavigationSection();
+	let navButtonList = buildNavigationButtonList(selectedPage, numberOfPageButtons);
+
+	navigationSection.append(navButtonList);
+
+	$("#landedTableId").after(navigationSection);
+	ActivatePageButton(selectedPage);
+
+	return navigationSection;
 }
+
